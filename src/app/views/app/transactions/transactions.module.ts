865b518eba0f6d2a6cb20d05892a3485fbbc7e79
Henry Johan Vargas <sactrom@gmail.com>
2022-07-25 16:25:20 -0500

    feat: tabs

--- src/app/views/app/transactions/transactions.module.ts
+++ src/app/views/app/transactions/transactions.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { MatTabsModule } from '@angular/material/tabs';
+
 import { TransactionsComponent } from './transactions.component';
+import { SummaryComponent } from './components/summary/summary.component';
 import { TransactionsRoutingModule } from './transactions-routing.module';
 import { SharedModule } from '@shared/shared.module';
-import { SummaryComponent } from './components/summary/summary.component';
+import { TabsComponent } from './components/tabs/tabs.component';
 
 
 
 @NgModule({
-  declarations: [TransactionsComponent, SummaryComponent],
+  declarations: [TransactionsComponent, SummaryComponent, TabsComponent],
   imports: [
     CommonModule,
     TransactionsRoutingModule,
-    SharedModule
+    SharedModule,
+    MatTabsModule
   ]
 })
 export class TransactionsModule { }
